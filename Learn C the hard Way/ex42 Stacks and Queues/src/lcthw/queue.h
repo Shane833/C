5be02497984d953b48c86e7d6e1c904ce875ac9a
Shane833 <williamsshane883@gmail.com>
2024-10-02 15:33:07 +0530

    Update queue.h

--- Learn C the hard Way/ex42 Stacks and Queues/src/lcthw/queue.h
+++ Learn C the hard Way/ex42 Stacks and Queues/src/lcthw/queue.h
@@ -10,4 +10,4 @@ typedef List Queue;
 #define Queue_peek List_first
 #define Queue_count List_count
 
-#define QUEUE_FOREACH(A, V) LIST_FOREACH(A, last, prev, V)
+#define QUEUE_FOREACH(A, V) LIST_FOREACH(A, last, prev, V)	
