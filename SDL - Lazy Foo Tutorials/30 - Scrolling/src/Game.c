5ca767548fcc43b23f5f4bd3ac570de1a8315a39
Shane833 <williamsshane883@gmail.com>
2025-02-06 18:05:07 +0530

    ex30 Scrolling

--- SDL - Lazy Foo Tutorials/30 - Scrolling/src/Game.c
+++ SDL - Lazy Foo Tutorials/30 - Scrolling/src/Game.c
@@ -1,8 +1,6 @@
 #include <SDL2/SDL_mixer.h> // Makes playing audio in the game easy
 #include <Game.h>
 #include <Texture.h>
-#include <UI.h>
-#include <Timer.h>
 #include <Dot.h>
 
 // Some globals
@@ -11,13 +9,16 @@ SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int LEVEL_WIDTH = 1280;
+const int LEVEL_HEIGHT = 960;
 TTF_Font* font = NULL;
 bool quit = false;
 
 // Texture Variable
 Texture dot_texture;
-Dot* dot;
-Dot* otherdot; 
+Texture bg_texture;
+Dot* dot = NULL;
+SDL_Rect camera = {0,0,0,0};
 
 int run()
 {
@@ -66,12 +67,17 @@ bool loadMedia()
 	bool r = Texture_loadFromFile(renderer, &dot_texture, "Assets/dot.bmp");
 	check(r != false, "Failed to load the texture!");
 	
+	r = Texture_loadFromFile(renderer, &bg_texture, "Assets/bg.png");
+	check(r != false, "Failed to load the bg texture!");
+	
 	dot = Dot_create(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 	check(dot != NULL, "Failed to create the dot!");
 	
-	otherdot = Dot_create(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4);
-	check(otherdot != NULL, "Failed to create the otherdot!");
-
+	camera.x = 0;
+	camera.y = 0;
+	camera.w = SCREEN_WIDTH;
+	camera.h = SCREEN_HEIGHT;
+	
 	return true;
 error:
 	return false;
@@ -93,10 +99,26 @@ void handleEvents()
 
 void update()
 {
-	Dot_move(dot, otherdot->colliders, SCREEN_WIDTH, SCREEN_HEIGHT);
+	// Move the dot
+	Dot_move(dot, LEVEL_WIDTH, LEVEL_HEIGHT);
 	
-error: // fallthrough
-	return;
+	// Center the camera over the dot
+	camera.x = (dot->position.x + DOT_WIDTH / 2) - SCREEN_WIDTH / 2;
+	camera.y = (dot->position.y + DOT_HEIGHT / 2) - SCREEN_HEIGHT / 2;
+	
+	// Keep the camera in bounds
+	if(camera.x < 0){
+		camera.x = 0;
+	}
+	if(camera.y < 0){
+		camera.y = 0;
+	}
+	if(camera.x > LEVEL_WIDTH - camera.w){
+		camera.x = LEVEL_WIDTH - camera.w;
+	}
+	if(camera.y > LEVEL_HEIGHT - camera.h){
+		camera.y = LEVEL_HEIGHT - camera.h;
+	}
 }
 
 void render()
@@ -104,12 +126,11 @@ void render()
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_RenderClear(renderer); // Clears the current frame
 	
-	// render the wall
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	// render the background
+	Texture_render(renderer, &bg_texture, 0, 0, &camera);
 	
-	// Render the dot
-	Dot_render(renderer, &dot_texture, otherdot);
-	Dot_render(renderer, &dot_texture, dot);
+	// render the dot
+	Dot_render(renderer, &dot_texture, dot, camera.x, camera.y);
 	
 	SDL_RenderPresent(renderer); // Display the frame to the screen
 }
@@ -133,4 +154,3 @@ void close()
 	IMG_Quit();
 	SDL_Quit();
 }
-
