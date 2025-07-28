130221a0c9541a108bb5d642b79fbeb839aee88f
Shane833 <williamsshane883@gmail.com>
2025-07-28 12:39:19 +0530

    started understanding ex38

---
+++ Curiosity Questions and Findings/20 - GDB/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	int a = 3;
+
+	printf("%d\n", a);
+
+	return 0;
+}
--- Learn C the hard Way/ex38 Hashmap Algorithms/src/lcthw/hashmap_algos.h
+++ Learn C the hard Way/ex38 Hashmap Algorithms/src/lcthw/hashmap_algos.h
@@ -2,11 +2,14 @@
 #define hashmap_algos_h
 
 #include <stdint.h>
+#include <lcthw/bstrlib.h>
 
-uint32_t Hashmap_fnv1a_hash(void* data);
+uint32_t Hashmap_fnv1a_hash(void * data, uint32_t * seed);
 
-uint32_t Hashmap_adler32_hash(void* data);
+uint32_t Hashmap_adler32_hash(void * data, uint32_t * seed);
 
-uint32_t Hashmap_djb_hash(void* data);
+uint32_t Hashmap_djb_hash(void * data, uint32_t * seed);
+
+uint32_t Hashmap_murmur3_hash(void * data, uint32_t * seed);
 
 #endif
